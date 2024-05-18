d8b95e3f69bddbf2a165e8968069a41b5ac89130
Adam Zielinski <github@maosmurf.com>
2024-05-18 12:03:06 +0200

    game repr

--- game.py
+++ game.py
@@ -4,10 +4,16 @@ The "ant" moves according to the rules below:
     At a white square, turn 90° clockwise, flip the color of the square, move forward one unit
     At a black square, turn 90° counter-clockwise, flip the color of the square, move forward one unit
 """
+class Game:
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return ">"
 
 
 def create_game() -> str:
-    return ">"
+    return str(Game())
 
 
 def move_ant_forward(game: str) -> str:
