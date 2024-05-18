22de484033133d55ac8b5727acdb4b520d9d9fd0
Adam Zielinski <github@maosmurf.com>
2024-05-18 11:03:40 +0200

    hello x

---
+++ test_game.py
@@ -0,0 +1,14 @@
+from approvaltests import verify, Options
+from approvaltests.inline.inline_options import InlineOptions
+
+options = Options().inline(InlineOptions.semi_automatic())
+
+from game import create_game
+
+
+def test_new_game():
+    """
+    x
+    """
+    game=create_game()
+    verify(game, options=options)
