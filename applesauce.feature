c6760c4cdea98dd1239848e933047c5a7d4907ac
Adam Zielinski <github@maosmurf.com>
2024-05-18 13:44:54 +0200

    gwt 1 step

--- applesauce.feature
+++ applesauce.feature
@@ -1,7 +1,12 @@
 Feature: Ant
 
   # The first example has two steps
-  Scenario: Fresh game
-    When the game starts
-    Then the board looks like ">"
+  Scenario Outline: Fresh game
+    Given the game starts with ">"
+    When <steps> run
+    Then the board looks like "<expected>"
+    Examples:
+      | steps | expected |
+      | 0     | >        |
+      | 1     | .v       |
 
--- test_applesauce.py
+++ test_applesauce.py
@@ -1,21 +1,34 @@
 import pytest
-from pytest_bdd import when, then, scenarios, parsers
+from pytest_bdd import when, then, scenarios, parsers, given
 
 from game import Game
 
 scenarios("applesauce.feature")
 
 
-@pytest.fixture(name="game")
-def fixture_game():
-    return Game(board=">")
+@pytest.fixture(name="foo")
+def fixture_foo() -> dict:
+    return dict()
+
+
+@given(parsers.parse('the game starts with "{start}"'))
+def fixture_game(foo, start):
+    foo['game'] = Game(board=start)
+
+
+@when(parsers.parse("{steps:d} run"))
+def steps_run(foo, steps):
+    game = foo['game']
+    for s in range(steps):
+        game = game.move()
+    foo['game'] = game
 
 
 @when("the game starts")
-def step_impl(game):
+def step_impl(foo):
     pass
 
 
 @then(parsers.parse('the board looks like "{expected}"'))
-def step_impl(game, expected: str):
-    assert expected == str(game)
+def step_impl(foo, expected: str):
+    assert expected == str(foo['game'])
