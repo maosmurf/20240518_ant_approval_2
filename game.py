1ef2d4ef1b556a8da97679c2cbaa1ff7471d9dca
Adam Zielinski <github@maosmurf.com>
2024-05-18 11:12:55 +0200

    move_ant_forward

--- game.py
+++ game.py
@@ -1,3 +1,14 @@
-def create_game():
+"""
+The "ant" moves according to the rules below:
+
+    At a white square, turn 90° clockwise, flip the color of the square, move forward one unit
+    At a black square, turn 90° counter-clockwise, flip the color of the square, move forward one unit
+"""
+
+
+def create_game() -> str:
     return ">."
 
+
+def move_ant_forward(game: str) -> str:
+    return ".>"
--- test_game.py
+++ test_game.py
@@ -3,12 +3,21 @@ from approvaltests.inline.inline_options import InlineOptions
 
 options = Options().inline(InlineOptions.semi_automatic())
 
-from game import create_game
+from game import create_game, move_ant_forward
 
 
 def test_new_game_has_space_to_the_right():
     """
     >.
     """
-    game=create_game()
+    game = create_game()
     verify(game, options=options)
+
+
+def test_move_ant_forward():
+    """
+    .>
+    """
+    game = create_game()
+    result = move_ant_forward(game=game)
+    verify(result, options=options)
